da0f8dfd8128780a04cfd951169598ea5cdf6dbe
Katsuomi Kobayashi <katsuomi.kobayashi@gmail.com>
2020-11-19 23:40:11 +0900

    Initial commit

---
+++ Assets/FrameSynthesis/UnityTools/Editor/UnityTools.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace FrameSynthesis
+{
+    public class UnityTools
+    {
+        const string ForkPath = "C:\\Users\\katsuomi\\AppData\\Local\\Fork\\Fork.exe";
+
+        [MenuItem("Frame Synthesis/Open Fork")]
+        public static void OpenFork()
+        {
+            Process.Start(ForkPath, Directory.GetParent(Application.dataPath).FullName);
+        }
+    }
+}
